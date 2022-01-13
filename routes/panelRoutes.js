464102a1d10533a3b0a203887edb50e8e0712374
ZenenAvila <zenenavilasuarez@gmail.com>
2022-01-12 19:32:33 -0600

    Modificacion de ruta arduino para mostrar 70 imagenes

--- routes/panelRoutes.js
+++ routes/panelRoutes.js
@@ -1,24 +1,292 @@
 const router = require('express').Router();
 const panelController=require('../dao/panelDao');
 
-router.get("/mostrararduino",async(request,response)=>{
+
+
+router.get("/mostrararduino1",async(request,response)=>{
     try{
         let respuesta=await panelController.mostrararduino();
         let listleds =respuesta.map(function(respuesta){
             return respuesta.leds
         });      
         let arduino="";
-        for (let led of listleds)
-        {
-            arduino+="-"+led;
+        try{
+            for(let i=0;i<7;i++)
+            {
+                arduino+="-"+listleds[i];
+            }
         }
+        catch(error){
+            console.log(`error mostrararduino(routers): ${error}`);
+        }
+        
         arduino=arduino.substring(1,arduino.length)
-        let arduinoJSON=JSON.parse('{"leds":"'+arduino+'"}');
+        let arduinoJSON="";
+        arduinoJSON=JSON.parse('{"leds":"'+arduino+'"}');
         response.json(arduinoJSON);
     } catch(error){
         console.log(`error mostrararduino(routers): ${error}`);
-    }
-     
+    }     
+});
+
+router.get("/mostrararduino2",async(request,response)=>{
+    try{
+        let respuesta=await panelController.mostrararduino();
+        let listleds =respuesta.map(function(respuesta){
+            return respuesta.leds
+        });      
+        let arduino="";
+        try{
+            for(let i=7;i<14;i++)
+            {
+                arduino+="-"+listleds[i];
+            }
+        }
+        catch(error){
+            console.log(`error mostrararduino(routers): ${error}`);
+        }
+        
+        arduino=arduino.substring(1,arduino.length)
+        let arduinoJSON="";
+        arduinoJSON=JSON.parse('{"leds":"'+arduino+'"}');
+        response.json(arduinoJSON);
+    } catch(error){
+        console.log(`error mostrararduino(routers): ${error}`);
+    }     
+});
+
+router.get("/mostrararduino3",async(request,response)=>{
+    try{
+        let respuesta=await panelController.mostrararduino();
+        let listleds =respuesta.map(function(respuesta){
+            return respuesta.leds
+        });      
+        let arduino="";
+        try{
+            for(let i=14;i<21;i++)
+            {
+                arduino+="-"+listleds[i];
+            }
+        }
+        catch(error){
+            console.log(`error mostrararduino(routers): ${error}`);
+        }
+        
+        arduino=arduino.substring(1,arduino.length)
+        let arduinoJSON="";
+        arduinoJSON=JSON.parse('{"leds":"'+arduino+'"}');
+        response.json(arduinoJSON);
+    } catch(error){
+        console.log(`error mostrararduino(routers): ${error}`);
+    }     
+});
+
+router.get("/mostrararduino4",async(request,response)=>{
+    try{
+        let respuesta=await panelController.mostrararduino();
+        let listleds =respuesta.map(function(respuesta){
+            return respuesta.leds
+        });      
+        let arduino="";
+        try{
+            for(let i=21;i<28;i++)
+            {
+                arduino+="-"+listleds[i];
+            }
+        }
+        catch(error){
+            console.log(`error mostrararduino(routers): ${error}`);
+        }
+        
+        arduino=arduino.substring(1,arduino.length)
+        let arduinoJSON="";
+        arduinoJSON=JSON.parse('{"leds":"'+arduino+'"}');
+        response.json(arduinoJSON);
+    } catch(error){
+        console.log(`error mostrararduino(routers): ${error}`);
+    }     
+});
+
+router.get("/mostrararduino5",async(request,response)=>{
+    try{
+        let respuesta=await panelController.mostrararduino();
+        let listleds =respuesta.map(function(respuesta){
+            return respuesta.leds
+        });      
+        let arduino="";
+        try{
+            for(let i=28;i<35;i++)
+            {
+                arduino+="-"+listleds[i];
+            }
+        }
+        catch(error){
+            console.log(`error mostrararduino(routers): ${error}`);
+        }
+        
+        arduino=arduino.substring(1,arduino.length)
+        let arduinoJSON="";
+        arduinoJSON=JSON.parse('{"leds":"'+arduino+'"}');
+        response.json(arduinoJSON);
+    } catch(error){
+        console.log(`error mostrararduino(routers): ${error}`);
+    }     
+});
+
+router.get("/mostrararduino6",async(request,response)=>{
+    try{
+        let respuesta=await panelController.mostrararduino();
+        let listleds =respuesta.map(function(respuesta){
+            return respuesta.leds
+        });      
+        let arduino="";
+        try{
+            for(let i=35;i<42;i++)
+            {
+                arduino+="-"+listleds[i];
+            }
+        }
+        catch(error){
+            console.log(`error mostrararduino(routers): ${error}`);
+        }
+        
+        arduino=arduino.substring(1,arduino.length)
+        let arduinoJSON="";
+        arduinoJSON=JSON.parse('{"leds":"'+arduino+'"}');
+        response.json(arduinoJSON);
+    } catch(error){
+        console.log(`error mostrararduino(routers): ${error}`);
+    }     
+});
+
+router.get("/mostrararduino7",async(request,response)=>{
+    try{
+        let respuesta=await panelController.mostrararduino();
+        let listleds =respuesta.map(function(respuesta){
+            return respuesta.leds
+        });      
+        let arduino="";
+        try{
+            for(let i=42;i<49;i++)
+            {
+                arduino+="-"+listleds[i];
+            }
+        }
+        catch(error){
+            console.log(`error mostrararduino(routers): ${error}`);
+        }
+        
+        arduino=arduino.substring(1,arduino.length)
+        let arduinoJSON="";
+        arduinoJSON=JSON.parse('{"leds":"'+arduino+'"}');
+        response.json(arduinoJSON);
+    } catch(error){
+        console.log(`error mostrararduino(routers): ${error}`);
+    }     
+});
+
+router.get("/mostrararduino8",async(request,response)=>{
+    try{
+        let respuesta=await panelController.mostrararduino();
+        let listleds =respuesta.map(function(respuesta){
+            return respuesta.leds
+        });      
+        let arduino="";
+        try{
+            for(let i=49;i<56;i++)
+            {
+                arduino+="-"+listleds[i];
+            }
+        }
+        catch(error){
+            console.log(`error mostrararduino(routers): ${error}`);
+        }
+        
+        arduino=arduino.substring(1,arduino.length)
+        let arduinoJSON="";
+        arduinoJSON=JSON.parse('{"leds":"'+arduino+'"}');
+        response.json(arduinoJSON);
+    } catch(error){
+        console.log(`error mostrararduino(routers): ${error}`);
+    }     
+});
+
+router.get("/mostrararduino9",async(request,response)=>{
+    try{
+        let respuesta=await panelController.mostrararduino();
+        let listleds =respuesta.map(function(respuesta){
+            return respuesta.leds
+        });      
+        let arduino="";
+        try{
+            for(let i=56;i<63;i++)
+            {
+                arduino+="-"+listleds[i];
+            }
+        }
+        catch(error){
+            console.log(`error mostrararduino(routers): ${error}`);
+        }
+        
+        arduino=arduino.substring(1,arduino.length)
+        let arduinoJSON="";
+        arduinoJSON=JSON.parse('{"leds":"'+arduino+'"}');
+        response.json(arduinoJSON);
+    } catch(error){
+        console.log(`error mostrararduino(routers): ${error}`);
+    }     
+});
+
+router.get("/mostrararduino10",async(request,response)=>{
+    try{
+        let respuesta=await panelController.mostrararduino();
+        let listleds =respuesta.map(function(respuesta){
+            return respuesta.leds
+        });      
+        let arduino="";
+        try{
+            for(let i=63;i<70;i++)
+            {
+                arduino+="-"+listleds[i];
+            }
+        }
+        catch(error){
+            console.log(`error mostrararduino(routers): ${error}`);
+        }
+        
+        arduino=arduino.substring(1,arduino.length)
+        let arduinoJSON="";
+        arduinoJSON=JSON.parse('{"leds":"'+arduino+'"}');
+        response.json(arduinoJSON);
+    } catch(error){
+        console.log(`error mostrararduino(routers): ${error}`);
+    }     
+});
+
+router.get("/mostrararduino2",async(request,response)=>{
+    try{
+        let respuesta=await panelController.mostrararduino();
+        let listleds =respuesta.map(function(respuesta){
+            return respuesta.leds
+        });      
+        let arduino="";
+        try{
+            for(let i=7;i<14;i++)
+            {
+                arduino+="-"+listleds[i];
+            }
+        }
+        catch(error){
+            console.log(`error mostrararduino(routers): ${error}`);
+        }
+        
+        arduino=arduino.substring(1,arduino.length)
+        let arduinoJSON="";
+        arduinoJSON=JSON.parse('{"leds":"'+arduino+'"}');
+        response.json(arduinoJSON);
+    } catch(error){
+        console.log(`error mostrararduino(routers): ${error}`);
+    }     
 });
 
 router.get("/mostrar",async(request,response)=>{
